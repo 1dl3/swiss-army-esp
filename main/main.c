c32b419eb8e1003bafae69c90517b1429e56b83e
bfuisz <benedikt.fuisz@mediprime.eu>
2026-01-16 10:31:55 +0100

    "works"

--- main/main.c
+++ main/main.c
@@ -3,20 +3,32 @@
 #include "freertos/task.h"
 #include "swiss_army_knife.h"
 
-void app_main(void) {
-    bsp_i2c_init();
-    bsp_display_start();
-    bsp_display_brightness_set(100);
-    
-    lv_init();
-    // Display + Touch init ist bei dir schon drin
-    // In einer echten BSP Umgebung müssten hier bsp_lvgl_port_init() etc. aufgerufen werden
-    // Ich setze voraus, dass das LVGL-Porting bereits im Hintergrund läuft oder initialisiert ist.
-
-    swiss_army_knife_init("/");   
-
-    while (1) {
+static void lvgl_task(void *arg)
+{
+    while (true) {
         lv_timer_handler();
         vTaskDelay(pdMS_TO_TICKS(5));
     }
+}
+
+void app_main(void)
+{
+    bsp_i2c_init();
+    bsp_display_start();
+    bsp_display_brightness_set(100);
+
+    // lv_init();
+    // bsp_lvgl_port_init();
+
+    swiss_army_knife_init("/");
+
+    xTaskCreate(
+        lvgl_task,
+        "lvgl",
+        4096,
+        NULL,
+        5,
+        NULL
+    );
+
 }
